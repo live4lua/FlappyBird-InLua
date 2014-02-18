5223588eb286cd504b3236645ea77b36ff91be9f
lucasgolino <lucasgolino@hotmail.com>
2014-02-17 22:29:55 -0300

    Iniciado o sistema de Pontos

--- game/engine/game.lua
+++ game/engine/game.lua
@@ -1,11 +1,16 @@
 engine.game = {}
 
 function engine.game.Config()
+	eGame = {}
+
+	eGame.points = 0 -- Pontos iniciados pelo jogo
 end
 
 function engine.game.GameOver()
 	bird.lock = 2
+	eGame.points = 0
 end
 
 function engine.game.GivePoint()
+	eGame.points = eGame.points + 1
 end
